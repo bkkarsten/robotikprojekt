1c3b39310577e28b948bc0de060e43fb5f53e71a
Jakob Kampers <jakobkampers23@gmail.com>
2025-02-20 12:49:54 +0100

    feat: basic implementation of main file

---
+++ src/MoleController.py
@@ -0,0 +1,3 @@
+class MoleController:
+    def start_routine(self) -> None:
+        print("Mole controller has started the routine!")
--- src/main.py
+++ src/main.py
@@ -1,6 +1,25 @@
-def main():
-    pass
+import asyncio
+from RoboterController import RoboterController
+from RoboterInterface import RoboterInterface, Communicator
+from MoleController import MoleController
+
+
+def setup() -> tuple[RoboterController, MoleController, RoboterInterface]:
+    """
+    This function initializes relevant objects.
+    :returns: tuple with initialized RoboterController, MoleController & RoboterInterface
+    """
+    return RoboterController(), MoleController(), Communicator()
+
+
+async def main() -> None:
+    roboter_controller: RoboterController
+    mole_controller: MoleController
+    roboter_interface: RoboterInterface
+    roboter_controller, mole_controller, roboter_interface = setup()
+
+    mole_controller.start_routine()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
