2511fed75a7c01573c912023deb35b8a3a6a2ca7
Jakob Kampers <jakobkampers23@gmail.com>
2025-02-23 16:31:32 +0100

    feat: MoleController init

--- src/MoleController.py
+++ src/MoleController.py
@@ -1,6 +1,47 @@
 import asyncio
 
 
+from datatypes import Mole
+from typing import List
+from RoboterInterface import RoboterInterface
+
+
 class MoleController:
-    async def update(self) -> None:
-        print("Mole controller has started the routine!")
+    def __init__(self,
+                 moles: List[Mole],
+                 min_waiting_time: float,
+                 max_waiting_time: float,
+                 max_moles: int,
+                 roboter_interface: RoboterInterface):
+
+        # parameter validation
+        if len(moles) == 0:
+            raise ValueError('moles must have at least one element')
+        if max_moles < 1:
+            raise ValueError(f'max_moles must be positive. Actual value {max_moles}')
+        if len(moles) < max_moles:
+            raise ValueError('Maximum number of moles is higher than the overall number of moles.')
+
+        if min_waiting_time < 0:
+            raise ValueError(f'min_waiting_time must be non-negative. Actual value: {min_waiting_time}')
+        if max_waiting_time < 0:
+            raise ValueError(f'max_waiting_time must be non-negative. Actual value: {max_waiting_time}')
+        if min_waiting_time > max_waiting_time:
+            swap: float = max_waiting_time
+            max_waiting_time = min_waiting_time
+            min_waiting_time = swap
+
+        self.moles: List[Mole] = moles
+        self._min_waiting_time: float = min_waiting_time
+        self._max_waiting_time: float = max_waiting_time
+        self._max_moles: int = max_moles
+        self._roboter_interface: RoboterInterface = roboter_interface
+        self._task: asyncio.Task | None = None
+
+    async def start_routine(self):
+        pass
+    async def update(self):
+        pass
+
+    def mole_hit(self):
+        pass
