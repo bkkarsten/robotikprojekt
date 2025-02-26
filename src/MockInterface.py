146620ad84c6d5d6036bbfa8bd943d09c0cc19b3
Jakob Kampers <jakobkampers23@gmail.com>
2025-02-26 17:03:43 +0100

    refactor: roboter_controller and mole_controller are now private members of MockInterface

--- src/MockInterface.py
+++ src/MockInterface.py
@@ -17,8 +17,9 @@ class MockInterface(RoboterInterface):
         """
         param randomised: Whether to move one mole to a random position every time get_moles is called
         """
-        self.roboter_controller = roboter_controller
-        self.mole_controller = mole_controller
+        self._tcp = Frame(Position(0, 0, 0), Orientation(0, 0, 0))
+        self._roboter_controller = roboter_controller
+        self._mole_controller = mole_controller
 
     async def set_mole(self, mole: Mole) -> None:
         if mole not in self.get_moles():
@@ -33,15 +34,21 @@ class MockInterface(RoboterInterface):
         print(f"Unset mole {mole}")
 
     async def move_tcp(self, frame: Frame) -> None:
-        self.tcp = frame
-        print(f"TCP moved to {self.tcp}")
+        self._tcp = frame
+        print(f"TCP moved to {self._tcp}")
 
     async def get_tcp(self) -> Frame:
-        return self.tcp
+        return self._tcp
 
     async def get_moles(self) -> List[Mole]:
-        return self.mole_controller.moles
+        return self._mole_controller.moles
 
     async def notify(self) -> None:
-        await self.roboter_controller.notify()
+        await self._roboter_controller.notify()
         print("Notified")
+
+    def set_roboter_controller(self, roboter_controller: RoboterController) -> None:
+        self._roboter_controller = roboter_controller
+
+    def set_mole_controller(self, mole_controller: MoleController) -> None:
+        self._mole_controller = mole_controller
