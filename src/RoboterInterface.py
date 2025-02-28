dbc953fa17c5823161cb8bf7b714127a82f5686e
bkk <bkarsten@unitybox.de>
2025-02-28 21:09:50 +0100

    refactor: remove mock Communicator class implementation

--- src/RoboterInterface.py
+++ src/RoboterInterface.py
@@ -34,25 +34,3 @@ class RoboterInterface(ABC):
     async def notify(self) -> None:
         pass
 
-
-class Communicator(RoboterInterface):
-    """
-    Mock interface implementation until the Communicator is implemented.
-    """
-    def set_mole(self, mole: types.Mole) -> None:
-        pass
-
-    def unset_mole(self, mole: types.Mole) -> None:
-        pass
-
-    def move_tcp(self, frame: types.Frame) -> None:
-        pass
-
-    def get_tcp(self) -> types.Frame:
-        return types.Frame()
-
-    def get_moles(self) -> List[types.Mole]:
-        return list()
-
-    def notify(self) -> None:
-        pass
