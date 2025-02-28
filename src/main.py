3d84da4ef3ef4d7c867978c8109df7974833879e
Jakob Kampers <jakobkampers23@gmail.com>
2025-02-28 09:52:30 +0100

    feat: setup method in main

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 import asyncio
+from typing import List, Tuple
+from datatypes import Position, Mole
 from RoboterController import RoboterController
 from RoboterInterface import RoboterInterface
 from MockInterface import MockInterface as Communicator
@@ -10,7 +12,25 @@ def setup() -> tuple[RoboterController, MoleController, RoboterInterface]:
     This function initializes relevant objects.
     :returns: tuple with initialized RoboterController, MoleController & RoboterInterface
     """
-    return RoboterController(), MoleController(), Communicator(randomised=True)
+    moles: List[Mole] = []
+    # home position of the simulation
+    home_position: Position = Position(2405, 5, 1000)
+
+    for x in range(-1, 2):
+        for y in range(-1, 2):
+            position: Position = Position(home_position.x + x * 100, home_position.y + y * 100, home_position.z)
+            moles.append(Mole(mole_id=x + y, position=position, isActive=False))
+
+    communicator: RoboterInterface = Communicator(randomised=True, roboter_controller=None, mole_controller=None)
+    roboter_controller: RoboterController = RoboterController(roboter_interface=communicator)
+    mole_controller: MoleController = MoleController(moles=moles,
+                                                     min_waiting_time=1,
+                                                     max_waiting_time=2,
+                                                     max_moles=1,
+                                                     roboter_interface=communicator)
+    communicator.roboter_controller = roboter_controller
+    communicator.mole_controller = mole_controller
+    return roboter_controller, mole_controller, communicator
 
 
 async def main() -> None:
